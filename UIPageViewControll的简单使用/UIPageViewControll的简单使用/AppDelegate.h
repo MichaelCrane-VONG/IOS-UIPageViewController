97e51ce72c13300ef8cce798bc1a747ff5a3defd
myApple <myapple@myapple.local>
2016-01-13 21:21:03 +0800

    create project

---
+++ UIPageViewControll的简单使用/UIPageViewControll的简单使用/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIPageViewControll的简单使用
+//
+//  Created by myApple on 16/1/13.
+//  Copyright © 2016年 myApple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIPageViewControll的简单使用/UIPageViewControll的简单使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIPageViewControll的简单使用
+//
+//  Created by myApple on 16/1/13.
+//  Copyright © 2016年 myApple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
